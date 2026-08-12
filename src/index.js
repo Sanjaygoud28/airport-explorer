b76b5d4bffb7ee41e0f37ca14d73b8ce254a91eb
Sanjaygoud28 <ailysanjay@gmail.com>
2026-08-12 23:48:45 +0530

    feat: connected Db

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-console.log("hello wold")
+import express from "express";
+import connectDb from "./config/mongodb.js";
+
+const app=express();
+app.use(express.json());
+const PORT = 8000;
+
+
+app.get("/", (req, res) => {
+  res.send("Server is running successfully");
+});
+async function startBackendApplication(){
+    await connectDb();
+    startHttpServer()
+}
+function startHttpServer(){
+    app.listen(PORT, async() => {
+            console.log("Server is running!", PORT)
+            console.log(`http://localhost:${PORT}`)
+    })
+}
+
+
+startBackendApplication();
