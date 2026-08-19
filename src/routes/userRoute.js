23d116697332ddae75316f280c4545b444f3225f
Ganesh Reddy <ganeshreddyg99@gmail.com>
2026-08-19 19:38:38 +0530

    Added validation and authentication changes

---
+++ src/routes/userRoute.js
@@ -0,0 +1,12 @@
+import express from "express";
+import {createUser, getMyProfile, getUser, getUserById, loginUser, logout, refreshAccessToken} from "../controllers/userController.js"
+const userRouter=express.Router();
+import { verifyAccessToken } from "../middlewares/auth.js";
+import { validate } from "../middlewares/validate.js";
+
+userRouter.post("/register",validate(createUserSchema),createUser)
+userRouter.post("/login",loginUser)
+userRouter.get("/logout",verifyAccessToken,logout)
+userRouter.get("/auth/refresh",refreshAccessToken)
+
+export default userRouter;
